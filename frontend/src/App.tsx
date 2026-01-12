c7cc9e50343f0d298ca73d17aa73974c649a15d1
손민식 <minmac@sonminsig-ui-MacBookPro.local>
2026-01-11 17:45:43 -0800

    feat: Add fontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </>
+  )
+}
+
+export default App
